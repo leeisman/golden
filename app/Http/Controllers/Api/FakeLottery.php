8c0b73bd0beed7b4b243b09bda821f63635d61b7
frankie <101m06008@stud.sju.edu.tw>
2020-01-31 18:31:45 +0800

    [feat] 第三信號源

--- app/Http/Controllers/Api/FakeLottery.php
+++ app/Http/Controllers/Api/FakeLottery.php
@@ -86,4 +86,55 @@ class FakeLottery extends Controller
             ]);
         }
     }
+
+    public function three()
+    {
+        if(\request('code') == 'cqssc'){
+            return response()->json([
+                "rows" => "3",
+                "code" => "cqssc",
+                "data" => [
+                    [
+                        "expect"   => "20190902003",
+                        "opencode" => "3,8,1,9,5",
+                        "opentime" => "2019-09-02 01:12:46"
+                    ],
+                    [
+                        "expect"   => "20190902002",
+                        "opencode" => "3,1,5,8,6",
+                        "opentime" => "2019-09-02 00:52:37"
+                    ],
+                    [
+                        "expect"   => "20190902001",
+                        "opencode" => "6,1,9,0,3",
+                        "opentime" => "2019-09-02 00:32:03"
+                    ]
+                ]
+            ]);
+        }
+
+        if(\request('code') == 'bj11x5'){
+            return response()->json([
+                "rows" => "3",
+                "code" => "bj11x5",
+                "data" => [
+                    [
+                        "expect"   => "20190902003",
+                        "opencode" => "3,8,1,9,5",
+                        "opentime" => "2019-09-02 01:12:46"
+                    ],
+                    [
+                        "expect"   => "20190902002",
+                        "opencode" => "3,1,5,8,6",
+                        "opentime" => "2019-09-02 00:52:37"
+                    ],
+                    [
+                        "expect"   => "20190902001",
+                        "opencode" => "5,1,9,0,3",
+                        "opentime" => "2019-09-02 00:32:03"
+                    ]
+                ]
+            ]);
+        }
+    }
 }
